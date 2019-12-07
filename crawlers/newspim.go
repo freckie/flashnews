11d9f5ae9bf4f29ad5a28bf6e55b1ea6897ca7cf
freckie <freckie@frec.kr>
2019-12-07 23:58:41 +0900

    Add: new newspim crawler

---
+++ crawlers/newspim.go
@@ -0,0 +1,111 @@
+package crawlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"flashnews/models"
+	"flashnews/utils"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const newspimCommonURL = "http://www.newspim.com/quicknews/left_list/?category_cd="
+const newspimItemURL = "http://www.newspim.com"
+
+type Newspim struct{}
+
+func (c Newspim) GetName() string {
+	return "newspim"
+}
+
+func (c Newspim) GetList(number int) ([]models.NewsItem, error) {
+	// Number
+	var _number int
+	if number > 15 || number < 1 {
+		_number = 15
+	} else {
+		_number = number
+	}
+	result := make([]models.NewsItem, _number)
+
+	// Request
+	req, err := http.Get(newspimCommonURL)
+	if err != nil {
+		return result, err
+	}
+	defer req.Body.Close()
+
+	if req.StatusCode != 200 {
+		return result, fmt.Errorf("[ERROR] Request Status Code : %d, %s", req.StatusCode, req.Status)
+	}
+
+	// HTML Load
+	html, err := goquery.NewDocumentFromReader(req.Body)
+	if err != nil {
+		return result, err
+	}
+
+	// Parsing
+	wrapper := html.Find("ul.newslist")
+	items := wrapper.Find("li")
+	items.Each(func(i int, sel *goquery.Selection) {
+		if i >= _number {
+			return
+		}
+
+		aTag := sel.Find("a")
+		href, ok := aTag.Attr("href")
+		if !ok {
+			result[i] = models.NewsItem{}
+			return
+		}
+		url := newspimItemURL + href
+
+		date := sel.Find("time").Text()
+		title := aTag.Find("span").Text()
+		if err != nil {
+			result[i] = models.NewsItem{}
+			return
+		}
+
+		result[i] = models.NewsItem{
+			Title:    title,
+			URL:      url,
+			Contents: "",
+			Datetime: date,
+		}
+	})
+
+	return result, nil
+}
+
+func (c Newspim) GetContents(item *models.NewsItem) error {
+	// Request
+	req, err := http.Get(item.URL)
+	if err != nil {
+		return err
+	}
+	defer req.Body.Close()
+
+	if req.StatusCode != 200 {
+		return fmt.Errorf("[ERROR] Request Status Code : %d, %s", req.StatusCode, req.Status)
+	}
+
+	// HTML Load
+	html, err := goquery.NewDocumentFromReader(req.Body)
+	if err != nil {
+		return err
+	}
+
+	// Parsing
+	wrapper := html.Find("div#news_contents")
+	contents := ""
+	wrapper.Find("p").Each(func(idx int, sel *goquery.Selection) {
+		contents += sel.Text()
+	})
+	contents = utils.TrimAll(contents)
+
+	item.Contents = contents
+	return nil
+}
