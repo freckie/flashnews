4ba57080953cb6af986d41f70c7d5d32be95503d
freckie <freckie@frec.kr>
2020-01-02 16:46:39 +0900

    Fix: iso8859-1 error

--- utils/encoding.go
+++ utils/encoding.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"unicode/utf8"
 
+	"golang.org/x/text/encoding/charmap"
+
 	"github.com/suapapa/go_hangul/encoding/cp949"
 )
 
@@ -27,14 +29,43 @@ func ReadCP949(data string) (string, error) {
 	return strings.TrimSpace(string(b[:c])), nil
 }
 
+func ReadCP949Bytes(data []byte) (string, error) {
+	br := bytes.NewReader(data)
+	r, err := cp949.NewReader(br)
+	if err != nil {
+		return "", err
+	}
+
+	b := make([]byte, 10*1024)
+
+	c, err := r.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(b[:c])), nil
+}
+
 func ReadISO88591(data string) (string, error) {
-	var iso8859_1_buf []byte
-	iso8859_1_buf = []byte(data)
+	iso8859_1_buf := []byte(data)
+
 	buf := make([]rune, len(iso8859_1_buf))
 	for i, b := range iso8859_1_buf {
 		buf[i] = rune(b)
 	}
-	return string(buf), nil
+
+	dec := charmap.ISO8859_1.NewEncoder()
+	out, err := dec.Bytes([]byte(string(buf)))
+	if err != nil {
+		return "", err
+	}
+
+	result, err := ReadCP949Bytes(out)
+	if err != nil {
+		return "", err
+	}
+
+	return string(result), nil
 }
 
 func StringSplit(data string, length int) string {
